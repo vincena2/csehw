89483dc6ce6f3bd69a8653796c0cbbe285ce2172
vincena2 <vincena2@miamioh.edu>
2017-02-26 13:45:39 -0500

    Add files via upload

---
+++ Calc.java
@@ -0,0 +1,28 @@
+package cse321;
+
+//Introduction to Software Testing
+//Authors: Paul Ammann & Jeff Offutt
+//Chapter 3; page ??
+//See CalcTest.java, DataDrivenCalcTest.java for JUnit tests
+
+public class Calc
+{
+static public int add (int a, int b)
+{
+   return a + b;
+}
+
+static public int subtract (int a, int b)
+{
+   return a - b;
+}
+
+static public int mulitply (int a, int b)
+{
+   return a * b;
+}
+static public float divide (int a, int b)
+{
+   return a/b;
+}
+}
